2ea843e55d3c46e36d2cb5a65ede540d85941d40
dekantis <dekantis@gmail.com>
2020-01-27 17:14:43 +0300

    [ex2-50] Проверка при деактивации товара

---
+++ local/php_interface/init.php
@@ -0,0 +1,18 @@
+<?php
+
+AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", "OnBeforeIBlockElementUpdateHandler");
+
+function OnBeforeIBlockElementUpdateHandler(&$arFields)
+{
+    if ($arFields["IBLOCK_ID"] == 2 && $arFields["ACTIVE"] == "N") {
+        var_dump($arFields["ID"]);//id элемента
+        $res = CIBlockElement::GetByID($arFields["ID"]);
+        $ar_res = $res->GetNext(false, false);
+        $count = $ar_res["SHOW_COUNTER"];
+        if ($count > 2) {
+            global $APPLICATION;
+            $APPLICATION->throwException("Товар невозможно деактивировать, у него $count просмотров");
+            return false;
+        }
+    }
+}
