46ea20e78cf799a48714ab821669df7e88caf577
dekantis <dekantis@gmail.com>
2020-01-27 17:27:18 +0300

    [ex2-93] Записывать в Журнал событий открытие не существующих страниц сайта

---
+++ local/php_interface/init.php
@@ -0,0 +1,18 @@
+<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+AddEventHandler("main", "OnAfterEpilog", Array("ErrorChekerClass", "check404error"));
+
+function check404error()
+{
+    if (defined("ERROR_404"))
+    {
+        global $APPLICATION;
+        CEventLog::Add(array(
+            "SEVERITY" => "info",
+            "AUDIT_TYPE_ID" => "ERROR_404",
+            "MODULE_ID" => "main",
+            "ITEM_ID" => "ERROR_404",
+            "DESCRIPTION" => $APPLICATION->GetCurPageParam(),
+        ));
+    }
+}
